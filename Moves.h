b32c71ce302ac89ec5cdc558b68d579b4c353294
Vinícius do Carmo Melício <vinicmelicio@gmail.com>
2022-12-15 10:41:20 -0300

    movimentos dependem da classe Moves

--- Moves.h
+++ Moves.h
@@ -2,88 +2,111 @@
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+#include "MoveHelper.h"
 using namespace std;
 
 class Moves
 {
-public:
+private:
     struct move
     {
-        vector<vector<int>> colorCycle;
-        vector<vector<int>> pieceCycle;
+        vector<vector<vector<int>>> perm;
+        void Print()
+        {
+            string firstLine = "";
+            string secondLine = "";
+            for (int i = 0; i < perm.size(); i++)
+            {
+                for (int j = 0; j < perm[i].size(); j++)
+                {
+                    for (int k = 0; k < perm[i][j].size(); k++)
+                    {
+                        if (k == 0)
+                            firstLine = firstLine + to_string(perm[i][j][k]);
+                        else if (k == 1)
+                            secondLine = secondLine + to_string(perm[i][j][k]);
+                    }
+                }
+                firstLine += " ";
+                secondLine += " ";
+            }
+            cout << endl;
+            cout << firstLine << endl;
+            cout << secondLine << endl;
+        }
+
+        void Set(vector<vector<vector<int>>> moves)
+        {
+            perm.resize(moves.size());
+            for (int i = 0; i < perm.size(); i++)
+            {
+                perm[i].resize(moves[i].size());
+                for (int j = 0; j < perm[i].size(); j++)
+
+                    for (int k = 0; k < moves[i][j].size(); k++)
+                        perm[i][j].push_back(moves[i][j][k]);
+            }
+        }
     };
+
     unordered_map<string, move> movesTable;
 
-    void Set(const string name, vector<vector<int>> colorCycle,
-             vector<vector<int>> pieceCycle)
+public:
+    void Set(const string name, vector<vector<vector<int>>> permutations)
     {
+        if (Find(name))
+            throw("ERROR: (Moves::Set) name found");
         move m;
-        m.colorCycle.resize(colorCycle.size());
-        for (int i = 0; i < colorCycle.size(); i++)
-        {
-            m.colorCycle[i].resize(colorCycle[i].size());
-            for (int j = 0; j < colorCycle[i].size(); j++)
-            {
-                m.colorCycle[i][j] = colorCycle[i][j];
-            }
-        }
-        m.pieceCycle.resize(pieceCycle.size());
-        for (int i = 0; i < pieceCycle.size(); i++)
-        {
-            m.pieceCycle[i].resize(pieceCycle[i].size());
-            for (int j = 0; j < pieceCycle[i].size(); j++)
-            {
-                m.pieceCycle[i][j] = pieceCycle[i][j];
-            }
-        }
+        m.Set(permutations);
         movesTable.insert({name, m});
     }
 
-    void GetColorCycle(vector<vector<int>>& cycle, const string name)
-{
-    if (Find(name))
+    void SetSkewbMove(const string name, vector<int> colors,
+                      vector<int> otherColors, vector<int> pieces)
     {
-        cycle.resize(movesTable[name].colorCycle.size());
+        if (Find(name))
+            throw("ERROR: (Moves::SetSkewbMove) name found");
+        vector<vector<vector<int>>> finalMatrix;
+        vector<int> allColors = {0, 1, 2, 3, 4, 5};
+        vector<int> allPieces = {0, 1, 2, 3, 4};
+        vector<int> otherPieces;
 
-        for (int i=0; i<cycle.size(); i++) {
-            cycle[i].resize(movesTable[name].colorCycle[i].size());
-            for (int j=0; j<cycle[i].size(); j++) {
-                cycle[i][j] = movesTable[name].colorCycle[i][j];
-            }
-        }
+        MoveHelper::CombineColorPiece(colors, pieces, finalMatrix);
+        MoveHelper::CombineColorPiece(otherColors, pieces, finalMatrix);
+
+        rotate(pieces.rbegin(), pieces.rbegin() + 2, pieces.rend());
+        MoveHelper::CombineColorPiece(otherColors, pieces, finalMatrix);
+
+        sort(pieces.begin(), pieces.end());
+        set_difference(allPieces.begin(), allPieces.end(), pieces.begin(), pieces.end(),
+                       inserter(otherPieces, otherPieces.begin()));
+        for (int i = 0; i < otherPieces.size(); i++)
+            MoveHelper::CombineColorPiece(otherColors, {otherPieces[i]}, finalMatrix);
+
+        move m;
+        m.Set(finalMatrix);
+        movesTable.insert({name, m});
     }
-    
-}
-void GetPieceCycle(vector<vector<int>>& cycle, const string name)
-{
-    if (Find(name))
+
+    vector<vector<vector<int>>> &Get(const string name)
     {
-        cycle.resize(movesTable[name].pieceCycle.size());
-
-        for (int i=0; i<cycle.size(); i++) {
-            cycle[i].resize(movesTable[name].pieceCycle[i].size());
-            for (int j=0; j<cycle[i].size(); j++) {
-                cycle[i][j] = movesTable[name].pieceCycle[i][j];
-            }
-        }
+        if (Find(name))
+            return movesTable[name].perm;
+        throw "ERROR: (Moves::Get) name not found";
     }
-    
-}
 
-   //Init(&p, num);
-   //if(p == NULL) {
-   //   /*Memory allocation failed */
-   //}
-
-    bool Find(const string name) {
+    bool Find(const string name)
+    {
         if (movesTable.find(name) != movesTable.end())
             return true;
-        else
-        {
-            cerr << "Move " << name << " not found" << endl;
-            return false;
-        }
+        return false;
     }
 
-
+    void Print(const string name)
+    {
+        if (Find(name))
+            movesTable[name].Print();
+        else
+            throw "ERROR: (Moves::Print) name not found";
+    }
 };
